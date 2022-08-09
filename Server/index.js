15801d85264a90b41cce22e7b0b70882e215659c
HOGWARTS\aditya <adityamkk@gmail.com>
2022-08-09 18:25:34 -0500

    Single server creation and README

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,8 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const urlObject = require('url');
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -951,6 +953,55 @@ function doesNameExist(url) {
     return false;
 }
 
+function serveUI(req, res) {
+    // parse URL
+    const parsedUrl = urlObject.parse(req.url);
+    // extract URL path
+    let pathname = `.${parsedUrl.pathname}`;
+    // based on the URL path, extract the file extension. e.g. .js, .doc, ...
+    const ext = path.parse(pathname).ext;
+    // maps file extension to MIME typere
+    const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword'
+    };
+
+    fs.exists(pathname, function (exist) {
+    if(!exist) {
+        // if the file is not found, return 404
+        res.statusCode = 404;
+        res.end(`File ${pathname} not found!`);
+        return;
+    }
+
+    // if is a directory search for index file matching the extension
+    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+        if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+        } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+        }
+    });
+});
+
+}
+
 //Server Response
 const server = http.createServer((req, res) => {
 
@@ -1038,9 +1089,10 @@ const server = http.createServer((req, res) => {
         
         //Refreshes gamestate
         default:  
-            res.writeHead(200, { 'Content-Type': 'application/json' , 'Access-Control-Allow-Origin': '*'});
-            res.write(JSON.stringify(findGame(getCode(req)).returnSecureGame(req)));  
-            res.end();
+            //res.writeHead(200, { 'Content-Type': 'application/json' , 'Access-Control-Allow-Origin': '*'});
+            //res.write(JSON.stringify(findGame(getCode(req)).returnSecureGame(req)));  
+            //res.end();
+            serveUI(req, res);
     }
 });
 
--- UI/vue.config.js
+++ UI/vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: './',
+  assetsDir: './',
   transpileDependencies: true,
   devServer: {
     host: '192.168.1.231',
